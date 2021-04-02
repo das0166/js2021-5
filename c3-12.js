688320e776d6dac3ba65cda958f899630b02e17a
das0166 <das0166@naver.com>
2021-04-02 17:35:51 +0900

    3월 30일 배운 내용 정리

---
+++ c3-12.js
@@ -0,0 +1,10 @@
+let number = 31;
+
+console.log(number % 2 == 0 ? "짝수" :"홀수");
+
+let test;
+test = test ? test : "초기화 해주세요.";
+console.log(test);
+
+number = number ? number : "초기화해주세요";
+console.log(number);
